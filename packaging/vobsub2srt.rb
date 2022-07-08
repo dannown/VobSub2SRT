54a7e64415c4551aab977abc6655d23eded808f6
Dann Teng-Lee Dempsey <dann.dempsey@booking.com>
2022-07-08 10:35:22 +0200

    Add fake sha256

--- packaging/vobsub2srt.rb
+++ packaging/vobsub2srt.rb
@@ -6,6 +6,7 @@ require 'formula'
 class Vobsub2srt < Formula
   head 'https://github.com/dannown/VobSub2SRT.git', :using => :git
   homepage 'https://github.com/dannown/VobSub2SRT'
+  sha254 fffffff
 
   depends_on 'cmake'
   depends_on 'tesseract'
